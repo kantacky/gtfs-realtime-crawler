d4919b53334276de48726e994ebd62e92fdc2813
Kanta Oikawa <k.oikawa@kantacky.com>
2024-04-20 23:34:32 +0900

    Update to collect all provided data

--- main.go
+++ main.go
@@ -53,23 +53,33 @@ func checkFeed(url string) {
 		log.Fatal(err)
 	}
 
+	fmt.Printf("GTFS Realtime Version: %s  \n", message.Header.GetGtfsRealtimeVersion())
+	fmt.Printf("Incrementality: %s  \n", message.Header.GetIncrementality())
 	timestamp := time.Unix(int64(message.Header.GetTimestamp()), 0)
 	timestampString := timestamp.Format("2006-01-02T15:04:05+09:00")
 	fmt.Printf("Timestamp: %s  \n", timestampString)
-	fmt.Printf("Incrementality: %s  \n", message.Header.GetIncrementality())
 	fmt.Printf("Number of entities: %d  \n", len(message.Entity))
 
-	fmt.Println("| ID | Timestamp | Vehicle ID | Position |")
-	fmt.Println("| :-- | :-- | :-- | :-- |")
+	fmt.Println("| ID | Trip ID | Route ID | Direction ID | Start Time | Start Date | Schedule Relationship | Vehicle ID | Vehicle Label | Latitude | Longitude | Current Stop Sequence | Stop ID | Timestamp |")
+	fmt.Println("| :-- | :-- | :-- | --: | :-- | :-- | :-- | :-- | :-- | :-- | :-- | --: | :-- | :-- |")
 
 	for _, entity := range message.Entity {
 		fmt.Printf(
-			"| %s | %s | %s | %f, %f |\n",
+			"| %s | %s | %s | %d | %s | %s | %s | %s | %s | %f | %f | %d | %s | %s |\n",
 			entity.GetId(),
-			time.Unix(int64(entity.Vehicle.GetTimestamp()), 0).Format("2006-01-02T15:04:05+09:00"),
+			entity.Vehicle.Trip.GetTripId(),
+			entity.Vehicle.Trip.GetRouteId(),
+			entity.Vehicle.Trip.GetDirectionId(),
+			entity.Vehicle.Trip.GetStartTime(),
+			entity.Vehicle.Trip.GetStartDate(),
+			entity.Vehicle.Trip.GetScheduleRelationship(),
 			entity.Vehicle.Vehicle.GetId(),
+			entity.Vehicle.Vehicle.GetLabel(),
 			entity.Vehicle.Position.GetLatitude(),
 			entity.Vehicle.Position.GetLongitude(),
+			entity.Vehicle.GetCurrentStopSequence(),
+			entity.Vehicle.GetStopId(),
+			time.Unix(int64(entity.Vehicle.GetTimestamp()), 0).Format("2006-01-02T15:04:05+09:00"),
 		)
 	}
 }
